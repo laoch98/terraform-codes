ae9600ac202a69b7e0c651ed78f5866bf616f5c8
laoch98 <31343760+laoch98@users.noreply.github.com>
2023-03-02 16:15:11 +0800

    Update Baselab_SrSetup.ps1

--- PowerShell/Baselab_SrSetup.ps1
+++ PowerShell/Baselab_SrSetup.ps1
@@ -1,6 +1,6 @@
 #Set Execution Policy to allow script to run
 Set-ExecutionPolicy Bypass -Scope Process -Force 
-#Choco install and Choco Apps
+#Choco install and Choco Apps. test 134
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install putty -y
